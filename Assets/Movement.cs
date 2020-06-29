81dcaa7bb4c58c0c23fbcba5c3fc5f18fd19608d
Ricardo Martinez <36496129+ram8290@users.noreply.github.com>
2020-06-29 00:45:58 -0700

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Transform tf; // A variable to hold our Transform component
+    void Start()
+    {
+        // Get the Transform Component
+        tf = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        // Move up every frame draw by adding 1 to the y of our position
+        tf.position = tf.position + Vector3.up; // Vector3.up is a preset Vector of (0,1,0) 
+                                                // There is also a Vector3.right (1,0,0) and Vector3.forward (0,0,1)
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# sprite-mover 2.0
+ gpe104 july20
